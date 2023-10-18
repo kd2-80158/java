833de684cb0e907e3dcf845e875cee1ce0eee1f9
kd2-80158 <chhimwalsaurabh@gmail.com>
2023-10-18 21:16:46 +0530

    Assignment 10 reuploaded with some changes in question-2

--- Assignment_10/Assignment_10_Question_2/src/StringWithMaxLength.java
+++ Assignment_10/Assignment_10_Question_2/src/StringWithMaxLength.java
@@ -1,27 +1,50 @@
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
-
-
-public class StringWithMaxLength {
+public class StringWithMaxLength{
+	
+	public static int maxLength(Collection<String> str)
+	{
+		Iterator<String> itr=str.iterator();
+		int max=0;
+		while(itr.hasNext())
+		{
+			String str1=itr.next();
+			if(str1!=null)
+			{
+				int length=str1.length();
+				if(length>max)
+				{
+					max=length;
+				}
+			}
+		}
+		return max;
+		
+	}
 
 	public static void main(String[] args) {
        
 		List<String> string = new ArrayList<String>();
 		string.add("Saurabh");
-		string.add("Sam");
+		string.add("Zeus");
 		string.add("Sameer");
 		string.add("Hanu");
 		string.add("Rahul");
 		string.add("Ronit");
 		string.add("Ester");
 		
-		String st = Collections.max(string);
+//		String st = Collections.max(string);
 		
-		System.out.print("String with maximum length is: ");
-		System.out.println(st);
+        int result=maxLength(string);
+        for(String st:string)
+        {
+        	if(result==st.length())
+        		System.out.println(st);
+        }
 
 	}
+	}
 
-}
